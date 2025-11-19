405b981f16763bee3aa2027ed2ce1d437f82dcc4
Georgi Pavlov <georgipavlov17@gmail.com>
2025-11-20 00:46:19 +0200

    importing two langugaes and translate the app

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-header',
@@ -15,11 +15,31 @@ import { TranslateModule } from '@ngx-translate/core';
       RouterLinkActive
     ]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit { 
+  selectedLanguage!: string | null;
+  isEnglish!: boolean;
 
-  constructor() { }
+  constructor( private translate: TranslateService ) { }
 
   ngOnInit(): void {
+     this.checkLanguage();
   }
 
+  setLanguage(language: string) { 
+    this.isEnglish = !this.isEnglish;
+    const lang = language == 'en' ? 'en' : 'de';
+    language === 'en' ? this.isEnglish = true : this.isEnglish = false;
+
+    this.selectedLanguage !== null ? lang == this.selectedLanguage : lang; 
+
+    lang == 'en' ? this.translate.use('en') : this.translate.use('de');
+
+    localStorage.setItem('selectedLanguage', lang);  
+  }
+
+  checkLanguage() {
+    ( this.selectedLanguage !== null && this.selectedLanguage == 'en' ) ? this.isEnglish = true : this.isEnglish = false;
+    this.selectedLanguage = localStorage.getItem('selectedLanguage');  
+    this.selectedLanguage !== null ? this.translate.use( this.selectedLanguage ) : this.translate.use( 'en' ); 
+  }
 }
